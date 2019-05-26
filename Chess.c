9b3597fdb1386b37fa8faa0c61a4bfe416daeb2b
Apricott <krol.michal99@gmail.com>
2019-05-26 16:48:01 +0200

    Added pawn move validation, fixed bug with capturing pieces

--- Chess.c
+++ Chess.c
@@ -2,14 +2,14 @@
 #include <string.h>
 
 void print_chessboard();
-void fill_chessboard(); // nie ma takiej funkcji
 void fill_chessman();
-void play(int x);
-int move(enum Color);
+void moves(int p, int r, int s, int o);
+int validate_move_format();
 
 enum Color { WHITE, BLACK };
-enum Type { PIONEK, SKOCZEK, GONIEC, HETMAN, WIEZA, KROL };
+enum Type { PAWN, KNIGHT, BISHOP, QUEEN, ROOK, KING };
 enum Dead_alive { DEAD, ALIVE };
+int rowStart, rowEnd, colStart, colEnd;
 
 typedef struct
 {
@@ -22,30 +22,30 @@ typedef struct
 }chessman;
 
 chessman pieces[32];
-int k, i;
+int k, i, move;
 
 void fill_chessman() {
 
 	for (k = 0; k < 8; k++)
-		pieces[k] = (chessman) { WHITE, PIONEK, 'P', k, 1 };
+		pieces[k] = (chessman) { WHITE, PAWN, 'O', k , 1 };
 	for (k = 16; k < 24; k++)
-		pieces[k] = (chessman) { BLACK, PIONEK, 'p', k - 16, 6 };
+		pieces[k] = (chessman) { BLACK, PAWN, 'o', k - 16, 6 };
 	for (k = 8; k < 10; k++)
-		pieces[k] = (chessman) { WHITE, WIEZA, 'W', (k - 8) * 7, 0 };
+		pieces[k] = (chessman) { WHITE, ROOK, 'R', (k - 8) * 7, 0 };
 	for (k = 10; k < 12; k++)
-		pieces[k] = (chessman) { WHITE, SKOCZEK, 'S', -49 + k * 5, 0 };
+		pieces[k] = (chessman) { WHITE, KNIGHT, 'N', -49 + k * 5, 0 };
 	for (k = 12; k < 14; k++)
-		pieces[k] = (chessman) { WHITE, GONIEC, 'G', k * 3 - 34, 0 };
-	pieces[14] = (chessman) { WHITE, HETMAN, 'H', 4, 0 };
-	pieces[15] = (chessman) { WHITE, KROL, 'K', 3, 0 };
+		pieces[k] = (chessman) { WHITE, BISHOP, 'B', k * 3 - 34, 0 };
+	pieces[14] = (chessman) { WHITE, QUEEN, 'Q', 4, 0 };
+	pieces[15] = (chessman) { WHITE, KING, 'K', 3, 0 };
 	for (k = 24; k < 26; k++)
-		pieces[k] = (chessman) { BLACK, WIEZA, 'w', (k - 24) * 7, 7 };
+		pieces[k] = (chessman) { BLACK, ROOK, 'r', (k - 24) * 7, 7 };
 	for (k = 26; k < 28; k++)
-		pieces[k] = (chessman) { BLACK, SKOCZEK, 's', k * 5 - 129, 7 };
+		pieces[k] = (chessman) { BLACK, KNIGHT, 'n', k * 5 - 129, 7 };
 	for (k = 28; k < 30; k++)
-		pieces[k] = (chessman) { BLACK, GONIEC, 'g', k * 3 - 82, 7 };
-	pieces[30] = (chessman) { BLACK, HETMAN, 'h', 4, 7 };
-	pieces[31] = (chessman) { BLACK, KROL, 'k', 3, 7 };
+		pieces[k] = (chessman) { BLACK, BISHOP, 'b', k * 3 - 82, 7 };
+	pieces[30] = (chessman) { BLACK, QUEEN, 'q', 4, 7 };
+	pieces[31] = (chessman) { BLACK, KING, 'k', 3, 7 };
 	for (k = 0; k < 32; k++)
 		pieces[k].dead_alive = ALIVE;
 }
@@ -75,52 +75,48 @@ void print_chessboard() {
 	}
 	printf("  h  g  f  e  d  c  b  a\n");
 }
-void play(int x) {
-
-}
 
 int main() {
 	int gra = 0;
+	move = 0;
 	fill_chessman();
-	enum Color enum_color = BLACK;
-	char str_color[5]; //uzywana do wyswietlenia koloru gracza podczas jego tury
 
 	do {
 
-		if (enum_color == BLACK) {
-			enum_color = WHITE;
-			strcpy_s(str_color, 6, "White"); // funkcja przypisujaca zmiennej str_color wartosc
-		}
-		else {
-			enum_color = BLACK;
-			strcpy_s(str_color, 6, "Black"); // funkcja przypisujaca zmiennej str_color wartosc
-		}
-		 
-		printf("%s's turn\n\n", str_color);
 		print_chessboard();
+		if (move % 2 == 0)
+			printf("White's ");
+		else
+			printf("Black's ");
 
-		while(validate_move_format(enum_color) == 0) { // tu warunkiem jest sprawdzanie poprawnosci wprowadzonej komendy
+		if (validate_move_format() == 0) { // tu warunkiem jest sprawdzanie poprawnosci wprowadzonej komendy
 			printf("Invalid move!\n");
 		}
-	
+		else if (validate_move(rowStart, rowEnd, colStart, colEnd) == 0) {
+			printf("Invalid move!\n");
+		}
+		else {
+			moves(rowStart, rowEnd, colStart, colEnd);
+			move++;
+		}
+
 	} while (gra == 0);
 	return 0;
 }
 
 
-int validate_move_format(enum_color) {
+int validate_move_format() {
 
 	char command[99]; // za duzo, aby uniknac overflow
-	int rowStart, rowEnd, colStart, colEnd;
 	int tempDigit; // uzywana do wyciagniecia cyfry z ciagu znakow
 
 	printf("Enter your move\n"); // Schemat: [start row][start col] to [end row][end col]
 	gets(command);
-	
+
 	if (strlen(command) != 8) { //sprawdza poprawna dlugosc wprowadzonej komendy
 		return 0;
 	}
-	
+
 
 	// zamiana liter na cyfry, aby mozna bylo ich uzywac jak indeksow
 
@@ -186,15 +182,30 @@ int validate_move_format(enum_color) {
 
 	for (k = 0; k < 32; k++) {
 		if (pieces[k].x == colStart && pieces[k].y == rowStart) {
-			if (pieces[k].color != enum_color) {
+			if ((pieces[k].color != WHITE && move % 2 == 0) || (pieces[k].color != BLACK && move % 2 == 1)){
 				printf("Cannot move enemy pawn!	");
 				return 0;
 			}
 		}
 	}
 
+	tempDigit = 0;
+	for (k = 0; k < 32; k++) {
+		if (pieces[k].x == colStart && pieces[k].y == rowStart)
+			tempDigit = 1;
+	}
+	if (tempDigit == 0)
+	{
+		printf("There's no pawn here. ");
+		return 0;
+	}
 
-	move(rowStart, rowEnd, colStart, colEnd); // W przyszlosci zastapione przez odwolanie do funkcji validate_move()
+	for (k = 0; k < 32; k++) {
+		if (pieces[k].x == colEnd && pieces[k].y == rowEnd && ((pieces[k].color == WHITE && move % 2 == 0) || (pieces[k].color == BLACK && move % 2 == 1))) {
+			printf("You have a pawn there. ");
+			return 0;
+		}
+	}
 
 	return 1;
 }
@@ -202,22 +213,108 @@ int validate_move_format(enum_color) {
 // FUNKCJA SPRAWDZAJACA LEGALOSC RUCHOW DLA POSZCZEGOLNYCH TYPOW PIONKOW
 int validate_move()
 {
+	//wektory Y i X
+	int rowDiff = rowStart - rowEnd;
+	int colDiff = colStart - colEnd;
 
+
+	/*
+	//Do testow
+	printf("rowDiff: %d \n", rowDiff);
+	printf("colDiff: %d \n", colDiff);
+	*/
+	
+	// Petla dla sprawdzenia typu bierki, ktora chcemy wykonac ruch
+	for (k = 0; k < 32; ++k)
+	{
+		//PIONKI
+		if ((pieces[k].y == rowStart && pieces[k].x == colStart) && pieces[k].type == PAWN) 
+		{
+			//Sprwdzenie ilosci pol, o ktore chcemy siê ruszyc
+			if ((rowStart != 1 && rowStart != 6) && rowDiff > 1) {
+				printf("Pawns cannot move more than one space except the first move.\n");
+				return 0;
+			}
+			else if ((rowStart == 1 || rowStart == 6) && rowDiff > 2) {
+				printf("Pawns can move up to two spaces in their first move.\n");
+				return 0;
+			}
+			//Wykluczenie ruchow do tylu
+			if ((pieces[k].color == BLACK) && rowDiff < 0) {
+				printf("Pawns cannot move backwards.\n");
+				return 0;
+			}
+			else if ((pieces[k].color == WHITE) && rowDiff > 0) {
+				printf("Pawns cannot move backwards.\n");
+				return 0;
+			}
+
+			//sprawdzanie, czy pionek nie wejdzie w inna figure
+			if (colDiff == 0) {
+				for (i = 0; i < 32; ++i) {
+					if (pieces[i].y == rowEnd && pieces[i].x == colEnd){
+						printf("Pawns do not move like that \n");
+						return 0;
+					}
+				}
+			}
+			
+			//Sprawdzanie, czy pionek moze wykonac ruch po przek¹tnej/w bok
+			if (colDiff != 0) {
+				for (i = 0; i < 32; ++i) {
+					if ((pieces[i].x == colEnd && pieces[i].y == rowEnd) && rowDiff != 0) {
+						return 1;
+					}
+				}
+				printf("Pawns do not move like that \n");
+				return 0;
+			}
+
+
+			/*
+			//DO TESTOW
+			printf("colStart: %d, colEnd: %d\n", colStart, colEnd);
+			printf("pieces[k].x: %d \n", pieces[k].x);
+			printf("rowStart: %d, rowEnd: %d\n", rowStart, rowEnd);
+			printf("pieces[k].y: %d \n", pieces[k].y);
+			*/
+
+		}
+
+	}
+
+	return 1;
 }
 
-int move(row_start, row_end, col_start, col_end)
+void moves(p, r, s, o)
 {
+	int row_for_killed;
 
 	// nadanie pionkom nowych wspolrzednych
 	for (k = 0; k < 32; k++) {
-		if (pieces[k].x == col_start && pieces[k].y == row_start) {
-				pieces[k].x = col_end;
-				pieces[k].y = row_end;
-				for (i = 0; i < 32; i++) {
-					if (pieces[k].x == pieces[i].x && pieces[k].y == pieces[i].y && k != i && pieces[k].color != pieces[i].color)
-						pieces[i].dead_alive = DEAD;
-					// usuwanie pionkow
+		if (pieces[k].x == s && pieces[k].y == p) {
+			pieces[k].x = o;
+			pieces[k].y = r;
+
+			for (i = 0; i < 32; i++) {
+				if (pieces[k].x == pieces[i].x && pieces[k].y == pieces[i].y && k != i && pieces[k].color != pieces[i].color) {
+					pieces[i].dead_alive = DEAD;
+					printf("Enemy piece captured! \n");
+
+					if (pieces[i].color == WHITE) {
+						row_for_killed = 0;
+					}
+					else {
+						row_for_killed = 1;
+					}
+					pieces[i].x = i + 10;
+					pieces[i].y = row_for_killed + 10;
+
+					// usuwanie pionkow (nowe wspolrzedne, aby nie zaklocac dalszej gry)
 				}
+			}
 		}
 	}
+
+	
 }
